d063e0845b10adb7af4a348b6fd779ca9d0a0f20
takushi <50552608+takushi-k@users.noreply.github.com>
2023-03-25 15:59:06 +0900

    initial commit

---
+++ pages/context-page.tsx
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout'
+import { StateProvider } from '../context/StateProvider'
+import ContextA from '../components/ContextA'
+import ContextB from '../components/ContextB'
+
+const ContextPage = () => {
+  return (
+    <Layout title="Context">
+      <p className="text-4xl mb-10">context page</p>
+      <StateProvider>
+        <ContextA />
+        <ContextB />
+      </StateProvider>
+    </Layout>
+  )
+}
+
+export default ContextPage
