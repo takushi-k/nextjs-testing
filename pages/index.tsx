23652cc712ac1aa6f8a89c400788725dc7ef29f2
taku <takutakushii@icloud.com>
2023-09-20 00:23:18 +0900

    c1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Home: React.FC = () => {
   return (
     <Layout title="Home">
-      <p className="text-4xl">Welcome to Nextjs</p>
+      <p className="text-4xl">Welcome to Nextjs2</p>
     </Layout>
   )
 }
